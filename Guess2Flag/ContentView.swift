8b931c60f9e4a9ab205419aa54d5a2c7f572fdab
turonueve <anlawrance@gmail.com>
2022-02-25 17:54:46 -0700

    Initial Commit

---
+++ Guess2Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess2Flag
+//
+//  Created by Arthur Lawrance on 2/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess2Flag/Guess2FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess2FlagApp.swift
+//  Guess2Flag
+//
+//  Created by Arthur Lawrance on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess2FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
